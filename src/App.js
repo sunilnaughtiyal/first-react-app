6a75c9200592ef309f3eabfbb0fc3a2067ce3773
SUNIL\sunil <sunil.sharma1@tothenew.com>
2025-07-27 21:50:31 +0530

    code updated for text file

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to my world.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.google.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Sunil
         </a>
       </header>
     </div>
