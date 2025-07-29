92c7314517de32875f228880c708ed17ac447091
SUNIL\sunil <sunil.sharma1@tothenew.com>
2025-07-29 10:40:44 +0530

    heading changed for the page

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome to my world.
+          Welcome to my real world.
         </p>
         {/*<a
           className="App-link"
