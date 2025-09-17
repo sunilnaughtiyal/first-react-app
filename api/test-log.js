e96d6c35bf7baff8db7fd9c710b6d2bbacd44770
SUNIL\sunil <sunil.sharma1@tothenew.com>
2025-09-17 21:28:29 +0530

    api removed

--- api/test-log.js
+++
@@ -1,4 +0,0 @@
-export default function handler(req, res) {
-  console.log("📩 Log from React project API:", new Date().toISOString());
-  res.status(200).json({ message: "Test log written from React project" });
-}
